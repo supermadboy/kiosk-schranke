930ed9266d495e82bbbb80fab08269c8000a4ec8
Marki <markusszar@gmail.com>
2021-09-25 23:53:48 +0200

    Add rebass

---
+++ packages/frontend/styles/styled.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components';
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string, 
+    },
+    buttons: {
+      primary: {
+        color: string,
+        bg: string,
+      }
+    }
+  }
+}
---
+++ packages/frontend/styles/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: 'red' ,
+  },
+  buttons: {
+    primary: {
+      color: 'gray',
+      bg: 'green'
+    }
+  }
+}
